0ceb076172ec9c65ab143029ec3c8cf87d52ae8d
tyler stuart <t.stu21.11@gmail.com>
2019-04-22 17:24:17 -0700

    first commit

---
+++ keys.js
@@ -0,0 +1,15 @@
+//Twitter Api key
+
+
+new twitterAPI({
+    consumerKey: 'aCF0oJY69v75aMAZ3YcFEARwN',
+    consumerSecret: '0U0Qhka222iuR6XcNaARKYyqLNLINXmkVkNQTCvQCY4vYjdqBa',
+    callback: 'http://placeholder.com',
+  });
+
+  
+  // Spotify
+
+var client_id = ' 4eff411427b24c36ad6c9fccffe676c2'; // Your client id
+var client_secret = 'b9363085581647608fefb7ef7ab877dc'; // Your secret
+var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
