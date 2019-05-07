55b257dfcfdd81b3dce8b86c8b76ebd3e5ddc8e1
tyler stuart <t.stu21.11@gmail.com>
2019-05-06 18:34:49 -0700

    added package.json

--- keys.js
+++ keys.js
@@ -2,14 +2,14 @@
 
 
 new twitterAPI({
-    consumerKey: 'aCF0oJY69v75aMAZ3YcFEARwN',
-    consumerSecret: '0U0Qhka222iuR6XcNaARKYyqLNLINXmkVkNQTCvQCY4vYjdqBa',
+    consumerKey: 'CONSUMER KEY',
+    consumerSecret: 'CONSUMER SECRET',
     callback: 'http://placeholder.com',
   });
 
   
   // Spotify
 
-var client_id = ' 4eff411427b24c36ad6c9fccffe676c2'; // Your client id
-var client_secret = 'b9363085581647608fefb7ef7ab877dc'; // Your secret
+var client_id = 'CLIENT_ID'; // Your client id
+var client_secret = 'CLIENT_SECRET'; // Your secret
 var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
